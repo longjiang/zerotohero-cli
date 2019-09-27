4d63de360566a8338ca589d4a93854cc89e91ee0
Jon Long <jon.long@voxelcreative.ca>
2019-09-26 21:03:35 -0700

    Add more channels and spanish youtube locale.

---
+++ src/lib/youtube-l2s/youtube-ara.js
@@ -0,0 +1,15 @@
+export default {
+  async channels() {
+    let channels = [
+      {
+        id: 'UCBq5hp6DvwWam2XOXw6lYJA',
+        title: 'Awakening Music',
+        description:
+          'Awakening Music is a pioneering and dynamic record label headquartered in London with offices in California and Cairo. Awakening introduced to the world global music sensations – the likes of Sami Yusuf, Maher Zain and Harris J. We produce faith inspired and values driven content. Our songs and music videos on Youtube and other digital platforms have been enjoyed by billions across the world.',
+        avatar:
+          'https://yt3.ggpht.com/a/AGF-l7-mgvEyq2tQ4SBuIx6p-uQ76uQCiqIzMwj5RA=s288-c-k-c0xffffffff-no-rj-mo'
+      }
+    ]
+    return channels
+  }
+}
---
+++ src/lib/youtube-l2s/youtube-fas.js
@@ -0,0 +1,24 @@
+export default {
+  async channels() {
+    let channels = [
+      {
+        id: 'UCbpFX1BatBHLMJ7DzUWyVGw',
+        title: 'Hambaazi',
+        description:
+          'Persian Children’s songs.',
+        avatar:
+          'https://yt3.ggpht.com/a/AGF-l7-MiWbSWu1cne6CUxpottqerKf8EaU03UNiRw=s288-c-k-c0xffffffff-no-rj-mo'
+      },
+      {
+        id: 'UCtr7DrT-BGQnst11NLCsv_w',
+        title: 'Persian Music',
+        description:
+          'Persian music videos.',
+        avatar:
+          'https://yt3.ggpht.com/a/AGF-l7-g6Sacx4d2PbJSECYs31Cwu_OAKKUPTm5w-A=s48-c-k-c0xffffffff-no-rj-mo'
+
+      }
+    ]
+    return channels
+  }
+}
