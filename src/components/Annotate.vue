1508d6b6f0ad4a1ced7ff1656443c7c5914c28aa
Jon Long <longjiang2005@gmail.com>
2019-11-05 10:25:34 -0800

    Some changes to locales and scripts.

--- src/components/Annotate.vue
+++ src/components/Annotate.vue
@@ -43,7 +43,6 @@
 import wordblock from '@/components/WordBlock'
 import VRuntimeTemplate from 'v-runtime-template'
 import TinySegmenter from 'tiny-segmenter'
-import ThaiWordcut from 'thai-wordcut'
 
 export default {
   components: {
@@ -131,7 +130,7 @@ export default {
     },
     async tokenize(text, batchId) {
       let html = text
-      if (['zh', 'yue', 'th', 'ja', 'km', 'ryu', 'bo'].includes(this.$l2.code)) {
+      if (['zh', 'yue', 'th', 'ja', 'km', 'ryu', 'bo', 'my'].includes(this.$l2.code)) {
         html = ''
         let tokenized = await (await this.$dictionary).tokenize(text)
         this.tokenized[batchId] = tokenized
