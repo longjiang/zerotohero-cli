371178cd1208cf97c45b1e562307d420df6a111b
Jon Long <longjiang2005@gmail.com>
2019-11-18 00:39:49 -0800

    Remove console log.

--- src/views/LessonVideos.vue
+++ src/views/LessonVideos.vue
@@ -133,7 +133,6 @@ export default {
     },
     async route() {
       let words = await (await this.$dictionary).lookupByLesson(this.level, this.lesson)
-      console.log(words)
       words = words.filter(word => !word.oofc || !word.oofc === '')
       if (this.$l2.han && this.$l2.code !== 'ja') {
         this.words = Helper.uniqueByValue(words, 'simplified')
