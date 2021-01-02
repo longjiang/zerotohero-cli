bbdf51ca939556267d1eb54cfaf65d53aebc79e6
Jon Long <longjiang2005@gmail.com>
2021-01-02 16:54:32 -0600

    Add ability to cycle through candidates.

--- src/components/WordBlockDictionary.vue
+++ src/components/WordBlockDictionary.vue
@@ -9,17 +9,18 @@
   >
     <span
       class="word-block-dictionary-simplified"
+      @click="cycleCandidate"
       :data-level="token.candidates[0].level"
       >{{ token.candidates[0].simplified }}</span
     >
     <span
       class="word-block-dictionary-pinyin"
       :data-level="token.candidates[0].level"
-      > ({{ token.candidates[0].pinyin }}) </span
+      > ({{ token.candidates[candidateIndex].pinyin }}) </span
     >
     <span
       class="word-block-dictionary-definition"
-    >{{ token.candidates[0].definitions.join(', ') }}. </span>
+    >{{ token.candidates[candidateIndex].definitions.join(', ') }}. </span>
   </span>
 </template>
 
@@ -39,8 +40,18 @@ export default {
     return {
       transliteration: undefined,
       id: `wordblock-dictionary-${Helper.uniqueId()}`,
+      candidateIndex: 0
     }
   },
+  methods: {
+    cycleCandidate() {
+      if (this.token && this.token.candidates && this.token.candidates.length > 0) {
+        let newCandidateIndex = this.candidateIndex + 1
+        if (newCandidateIndex > this.token.candidates.length - 1) newCandidateIndex = 0
+        this.candidateIndex = newCandidateIndex
+      }
+    }
+  }
 }
 </script>
 
