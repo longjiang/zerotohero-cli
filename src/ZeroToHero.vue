5ff466cb11bf54f032e145cee0f73714314a029b
Jon Long <longjiang2005@gmail.com>
2021-03-22 11:05:53 -0700

    Do not load home page unless we want it

--- src/ZeroToHero.vue
+++ src/ZeroToHero.vue
@@ -1,65 +1,86 @@
 <template>
   <div id="zerotohero" :class="classes">
-    <template v-if="langsLoaded && this.$route.path !== '/'">
-      <div class="container-fluid p-2 pl-3 site-top-bar">
-        <div>
-          <a href="/" style="color: #ccc; line-height: 2.3rem;"><i class="fa fa-chevron-left mr-2"></i>Zero to Hero Education</a>
+    <template v-if="this.$route.path !== '/'">
+      <template v-if="langsLoaded">
+        <div class="container-fluid p-2 pl-3 site-top-bar">
+          <div>
+            <a href="/" style="color: #ccc; line-height: 2.3rem;"
+              ><i class="fa fa-chevron-left mr-2"></i>Zero to Hero Education</a
+            >
+          </div>
+          <a
+            v-if="$l1.code === 'zh' && $l2.code === 'en'"
+            class="btn btn-sign-in text-white ml-1"
+            style="background-color: #2C5AFF"
+            href="https://m.cctalk.com/inst/stevmab3"
+            target="_blank"
+          >
+            登陆
+            <img src="/img/logo-cctalk-white.png" class="logo-small ml-1" />
+          </a>
+          <a
+            v-if="$l1.code === 'en' && $l2.code === 'zh'"
+            class="btn btn-primary btn-sign-in text-white ml-1"
+            href="https://sso.teachable.com/secure/133035/users/sign_in"
+          >
+            Login to
+            <img src="/img/teachable_light.png" class="logo-small" />
+          </a>
         </div>
-        <a
-          v-if="$l1.code === 'zh' && $l2.code === 'en'"
-          class="btn btn-sign-in text-white ml-1"
-          style="background-color: #2C5AFF"
-          href="https://m.cctalk.com/inst/stevmab3"
-          target="_blank"
-        >
-          登陆
-          <img src="/img/logo-cctalk-white.png" class="logo-small ml-1" />
-        </a>
-        <a
-          v-if="$l1.code === 'en' && $l2.code === 'zh'"
-          class="btn btn-primary btn-sign-in text-white ml-1"
-          href="https://sso.teachable.com/secure/133035/users/sign_in"
-        >
-          Login to
-          <img src="/img/teachable_light.png" class="logo-small" />
-        </a>
-      </div>
-      <div class="container-fluid bg-dark pl-0 pr-0">
-        <div class="container">
+        <div class="container-fluid bg-dark pl-0 pr-0">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 text-center">
+                <a
+                  v-if="this.$l1.code === 'en' && this.$l2.code === 'zh'"
+                  href="/en/zh"
+                  ><img
+                    src="/img/czh-logo-light.png"
+                    alt="Chinese Zero to Hero"
+                    style="max-width: 11rem; margin: 1.5rem 0"
+                /></a>
+                <a
+                  v-else-if="this.$l1.code === 'zh' && this.$l2.code === 'en'"
+                  href="/zh/en"
+                  ><img
+                    src="/img/ezh-logo-light.png"
+                    alt="Chinese Zero to Hero"
+                    style="max-width: 11rem; margin: 1.5rem 0"
+                /></a>
+                <LanguageLogo
+                  v-else
+                  :l1="$l1"
+                  :l2="$l2"
+                  style="margin-top: 1.5rem"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="container-fluid bg-dark sticky" style="z-index: 3">
           <div class="row">
-            <div class="col-sm-12 text-center">
-              <a v-if="this.$l1.code === 'en' && this.$l2.code === 'zh'" href="/en/zh"><img src="/img/czh-logo-light.png" alt="Chinese Zero to Hero" style="max-width: 11rem; margin: 1.5rem 0" /></a>
-              <a v-else-if="this.$l1.code === 'zh' && this.$l2.code === 'en'" href="/zh/en"><img src="/img/ezh-logo-light.png" alt="Chinese Zero to Hero" style="max-width: 11rem; margin: 1.5rem 0" /></a>
-              <LanguageLogo v-else :l1="$l1" :l2="$l2" style="margin-top: 1.5rem" />
-            </div>
+            <Nav />
           </div>
         </div>
-      </div>
-      <div class="container-fluid bg-dark sticky" style="z-index: 3">
-        <div class="row">
-          <Nav />
-        </div>
-      </div>
-      <SubNav class="pt-4" />
+        <SubNav class="pt-4" />
 
-      <keep-alive>
-        <router-view ref="routerView" />
-      </keep-alive>
+        <keep-alive>
+          <router-view ref="routerView" />
+        </keep-alive>
 
-      <footer class="container-fluid bg-dark text-light pt-4 pb-4">
-        <div class="container">
-          <div class="row mb-5">
-            <div class="col-sm-12">
-              <Choose :compact="true"/>
+        <footer class="container-fluid bg-dark text-light pt-4 pb-4">
+          <div class="container">
+            <div class="row mb-5">
+              <div class="col-sm-12">
+                <Choose :compact="true" />
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
+        </footer>
+      </template>
     </template>
     <template v-else>
-      <div
-        class="container-fluid pt-4"
-      >
+      <div class="container-fluid pt-4">
         <div class="bg-stars" />
         <div class="container">
           <div class="row pt-5">
@@ -67,57 +88,125 @@
               <img src="/img/logo-z2h.png" class="z2h-logo" />
             </div>
             <div class="col-sm-6">
-              <img src="/img/language-education-done-right.png" class="z2h-slogan img-fluid d-none d-sm-block" />
+              <img
+                src="/img/language-education-done-right.png"
+                class="z2h-slogan img-fluid d-none d-sm-block"
+              />
             </div>
           </div>
           <div class="row pt-5">
             <div class="col-sm-6">
               <div class="home-card">
-                <a href="/en/zh"><img src="/img/czh-logo-dark.png" class="czh-logo" /></a>
-                <hr/>
+                <a href="/en/zh"
+                  ><img src="/img/czh-logo-dark.png" class="czh-logo"
+                /></a>
+                <hr />
                 <ul class="czh-links">
-                  <li><a href="/en/zh/online-courses" style="color: #FD4F1C; font-weight: bold">HSK courses</a></li>
+                  <li>
+                    <a
+                      href="/en/zh/online-courses"
+                      style="color: #FD4F1C; font-weight: bold"
+                      >HSK courses</a
+                    >
+                  </li>
                   <li><a href="/en/zh/dictionary">Dictionary</a></li>
                   <li><a href="/en/zh/grammar">Grammar reference</a></li>
-                  <li><a href="/en/zh/youtube/browse">Audio-visual tools</a></li>
+                  <li>
+                    <a href="/en/zh/youtube/browse">Audio-visual tools</a>
+                  </li>
                   <li><a href="/en/zh/reader">Reading tools</a></li>
                   <li><a href="/en/zh/resource/list/all/all">Resources</a></li>
                 </ul>
-                <hr/>
-                <div class="home-card-logos text-center" style="line-height: 2rem;">
-                  <a href="https://www.youtube.com/channel/UCQ3IlLg5VGeydxtswBoyt6A"><img src="/img/logo-youtube.png" style="height: 1.2rem; padding: 0 0.5rem" /></a>
-                  <a href="https://www.instagram.com/chinesezerotohero/"><img src="/img/logo-instagram.png" style="height: 1.4rem; padding: 0 0.5rem" /></a>
-                  <a href="http://chinesezerotohero.teachable.com/"><img src="/img/logo-teachable.png" style="height: 1.4rem; padding: 0 0.5rem" /></a>
-                  <a href="http://t.me/zerotohero_zh"><img src="/img/logo-telegram.png" style="height: 1.4rem; padding: 0 0.5rem" /></a>
+                <hr />
+                <div
+                  class="home-card-logos text-center"
+                  style="line-height: 2rem;"
+                >
+                  <a
+                    href="https://www.youtube.com/channel/UCQ3IlLg5VGeydxtswBoyt6A"
+                    ><img
+                      src="/img/logo-youtube.png"
+                      style="height: 1.2rem; padding: 0 0.5rem"
+                  /></a>
+                  <a href="https://www.instagram.com/chinesezerotohero/"
+                    ><img
+                      src="/img/logo-instagram.png"
+                      style="height: 1.4rem; padding: 0 0.5rem"
+                  /></a>
+                  <a href="http://chinesezerotohero.teachable.com/"
+                    ><img
+                      src="/img/logo-teachable.png"
+                      style="height: 1.4rem; padding: 0 0.5rem"
+                  /></a>
+                  <a href="http://t.me/zerotohero_zh"
+                    ><img
+                      src="/img/logo-telegram.png"
+                      style="height: 1.4rem; padding: 0 0.5rem"
+                  /></a>
                 </div>
               </div>
             </div>
             <div class="col-sm-6">
               <div class="home-card">
-                <a href="/zh/en"><img src="/img/ezh-logo-dark.png" class="ezh-logo" /></a>
-                <hr/>
+                <a href="/zh/en"
+                  ><img src="/img/ezh-logo-dark.png" class="ezh-logo"
+                /></a>
+                <hr />
                 <ul class="czh-links">
-                  <li><a href="/zh/en/online-courses" style="color: #1B3E76; font-weight: bold">剑桥英语视频教程</a></li>
-                  <li><a href="/zh/en/online-courses" style="color: #1B3E76; font-weight: bold">美式口语课程</a></li>
+                  <li>
+                    <a
+                      href="/zh/en/online-courses"
+                      style="color: #1B3E76; font-weight: bold"
+                      >剑桥英语视频教程</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="/zh/en/online-courses"
+                      style="color: #1B3E76; font-weight: bold"
+                      >美式口语课程</a
+                    >
+                  </li>
                   <li><a href="/zh/en/dictionary">词典工具</a></li>
                   <li><a href="/zh/en/youtube/browse">视听工具</a></li>
                   <li><a href="/zh/en/reader">阅读工具</a></li>
                   <li><a href="/zh/en/resource/list/all/all">其它资源</a></li>
                 </ul>
-                <hr/>
-                <div class="home-card-logos text-center" style="line-height: 2rem;">
-                  <a href="https://m.cctalk.com/inst/stevmab3"><img src="/img/logo-cctalk.png" style="height: 1.2rem" /></a>&nbsp;&nbsp;
-                  <a href="https://space.bilibili.com/253569339"><img src="/img/logo-bilibili.png" style="height: 1.2rem" /></a>&nbsp;&nbsp;
-                  <a href="https://v.douyin.com/eNJCcD8/"><img src="/img/logo-douyin.png" style="height: 1.4rem" /></a>&nbsp;&nbsp;
-                  <a href="https://www.xiaohongshu.com/user/profile/5fad77c0000000000100696e"><img src="/img/logo-xiaohongshu.png" style="height: 1.4rem" /></a>&nbsp;&nbsp;
-                  <a href="/zh/en/contact-us"><img src="/img/logo-wechat.png" style="height: 1.4rem" /></a>
+                <hr />
+                <div
+                  class="home-card-logos text-center"
+                  style="line-height: 2rem;"
+                >
+                  <a href="https://m.cctalk.com/inst/stevmab3"
+                    ><img src="/img/logo-cctalk.png" style="height: 1.2rem"/></a
+                  >&nbsp;&nbsp;
+                  <a href="https://space.bilibili.com/253569339"
+                    ><img
+                      src="/img/logo-bilibili.png"
+                      style="height: 1.2rem"/></a
+                  >&nbsp;&nbsp;
+                  <a href="https://v.douyin.com/eNJCcD8/"
+                    ><img src="/img/logo-douyin.png" style="height: 1.4rem"/></a
+                  >&nbsp;&nbsp;
+                  <a
+                    href="https://www.xiaohongshu.com/user/profile/5fad77c0000000000100696e"
+                    ><img
+                      src="/img/logo-xiaohongshu.png"
+                      style="height: 1.4rem"/></a
+                  >&nbsp;&nbsp;
+                  <a href="/zh/en/contact-us"
+                    ><img src="/img/logo-wechat.png" style="height: 1.4rem"
+                  /></a>
                 </div>
               </div>
             </div>
           </div>
           <div class="row mt-5 mb-4 text-dark">
             <div class="col-sm-12 text-center">
-              <p style="font-size: 1.5rem">Resources for learning <strong><em>hundreds</em></strong> of languages</p>
+              <p style="font-size: 1.5rem">
+                Resources for learning <strong><em>hundreds</em></strong> of
+                languages
+              </p>
               <p>学习<strong>数百种</strong>语言的资源</p>
             </div>
           </div>
@@ -155,18 +244,21 @@ export default {
       languages: [],
       langsLoaded: false,
       focus: false,
-      loaded: false,
+      loaded: false
     }
   },
   methods: {
     loadSettings() {
-      Vue.prototype.$settings = Object.assign({
-        showDefinition: false,
-        showTranslation: true,
-        showPinyin: true,
-        useTraditional: false,
-        showQuiz: true,
-      }, JSON.parse(localStorage.getItem('zthSettings')))
+      Vue.prototype.$settings = Object.assign(
+        {
+          showDefinition: false,
+          showTranslation: true,
+          showPinyin: true,
+          useTraditional: false,
+          showQuiz: true
+        },
+        JSON.parse(localStorage.getItem('zthSettings'))
+      )
     },
     async loadLanguages() {
       Vue.prototype.$hasFeature = feature => {
@@ -188,16 +280,9 @@ export default {
           l2: this.$l2['iso639-3']
         })
       }
-      if (
-        !Vue.prototype.$hanzi &&
-        ['zh', 'ko', 'ja'].includes(this.$l2.code)
-      ) {
-        Vue.prototype.$hanzi = (await import(
-          `@/lib/hanzi.js`
-        )).default.load()
-        Vue.prototype.$unihan = (await import(
-          `@/lib/unihan.js`
-        )).default.load()
+      if (!Vue.prototype.$hanzi && ['zh', 'ko', 'ja'].includes(this.$l2.code)) {
+        Vue.prototype.$hanzi = (await import(`@/lib/hanzi.js`)).default.load()
+        Vue.prototype.$unihan = (await import(`@/lib/unihan.js`)).default.load()
       }
       if (!Vue.prototype.$grammar && ['zh'].includes(this.$l2.code)) {
         Vue.prototype.$grammar = (await import(
@@ -296,7 +381,7 @@ export default {
 .home-card {
   border-radius: 1rem;
   background-color: white;
-  box-shadow: 5px 5px 10px rgba(0,0,0,0.2);
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
   padding: 2rem;
   margin-bottom: 2rem;
 }
@@ -321,14 +406,16 @@ export default {
   height: 6rem;
 }
 
-.czh-links, .ezh-links {
+.czh-links,
+.ezh-links {
   column-count: 2;
   column-gap: 1rem;
   padding: 0;
   list-style: none;
 }
 
-.czh-links a, .ezh-links a {
+.czh-links a,
+.ezh-links a {
   color: #666;
   font-size: 0.8em;
 }
@@ -344,7 +431,8 @@ export default {
   right: 1rem;
 }
 
-.czh-logo, .ezh-logo {
+.czh-logo,
+.ezh-logo {
   max-width: 70%;
   margin: 0 auto;
   display: block;
