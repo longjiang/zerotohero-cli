ad7950e19ee8ad648331d57dcd7ba8123848cac1
Jon Long <longjiang2005@gmail.com>
2021-04-06 10:52:34 -0700

    Concordance should only show complete sentences.

--- src/components/Concordance.vue
+++ src/components/Concordance.vue
@@ -1,18 +1,21 @@
 <template>
   <div :key="'concordance-' + concordanceKey">
-    <div class="widget-title">{{ $t('Sentences with “{text}”', {text: term}) }}</div>
+    <div class="widget-title">
+      {{ $t('Sentences with “{text}”', { text: term }) }}
+    </div>
     <div class="jumbotron-fluid bg-light p-4">
       <div v-if="examples && examples.length > 0">
-        <ul v-if="examples" class="collapsed list-unstyled" data-collapse-target>
+        <ul
+          v-if="examples"
+          class="collapsed list-unstyled"
+          data-collapse-target
+        >
           <li v-for="example in examples">
             <Annotate tag="div" class="pt-2 pb-2" :showTranslate="true">
               <span
+                v-for="sentence of example.sentences"
                 v-html="
-                  Helper.highlightMultiple(
-                    example.l2,
-                    words,
-                    level || 'outside'
-                  )
+                  Helper.highlightMultiple(sentence, words, level || 'outside')
                 "
               />
             </Annotate>
@@ -20,7 +23,11 @@
             <hr />
           </li>
         </ul>
-        <ShowMoreButton :length="examples.length" :min="4" :data-bg-level="level" />
+        <ShowMoreButton
+          :length="examples.length"
+          :min="7"
+          :data-bg-level="level"
+        />
       </div>
       <div v-if="examples && examples.length === 0">
         Sorry, we could not find any “{{ term }}” examples. You can set a
@@ -31,32 +38,30 @@
       <div class="mt-4">
         <p>{{ $t('Sentences provided by') }}</p>
         <a
-          :href="
-            `https://app.sketchengine.eu/#concordance?corpname=${encodeURIComponent(
-              SketchEngine.corpname
-            )}&tab=basic&keyword=${
-              term
-            }&structs=s%2Cg&refs=%3Ddoc.website&showresults=1&operations=%5B%7B%22name%22%3A%22iquery%22%2C%22arg%22%3A%22${
-              term
-            }%22%2C%22active%22%3Atrue%2C%22query%22%3A%7B%22queryselector%22%3A%22iqueryrow%22%2C%22iquery%22%3A%22${
-              term
-            }%22%7D%2C%22id%22%3A3859%7D%5D`
-          "
+          :href="`https://app.sketchengine.eu/#concordance?corpname=${encodeURIComponent(
+            SketchEngine.corpname
+          )}&tab=basic&keyword=${term}&structs=s%2Cg&refs=%3Ddoc.website&showresults=1&operations=%5B%7B%22name%22%3A%22iquery%22%2C%22arg%22%3A%22${term}%22%2C%22active%22%3Atrue%2C%22query%22%3A%7B%22queryselector%22%3A%22iqueryrow%22%2C%22iquery%22%3A%22${term}%22%7D%2C%22id%22%3A3859%7D%5D`"
           target="_blank"
         >
-          <img src="/img/logo-sketch-engine.png" alt="Sketch Engine" class="ml-2 logo-small" />
+          <img
+            src="/img/logo-sketch-engine.png"
+            alt="Sketch Engine"
+            class="ml-2 logo-small"
+          />
         </a>
       </div>
       <hr />
       <div>
         <p>{{ $t('Search for more sentences at') }}</p>
         <a
-          :href="
-            `https://tatoeba.org/eng/sentences/search?from=${$l2['iso639-3']}&to=${$l1['iso639-3']}&query=${term}`
-          "
+          :href="`https://tatoeba.org/eng/sentences/search?from=${$l2['iso639-3']}&to=${$l1['iso639-3']}&query=${term}`"
           target="_blank"
         >
-          <img src="/img/logo-tatoeba.png" alt="Tatoeba" class="logo-small mr-3" />
+          <img
+            src="/img/logo-tatoeba.png"
+            alt="Tatoeba"
+            class="logo-small mr-3"
+          />
           Tatoeba
         </a>
       </div>
@@ -71,14 +76,14 @@ import SketchEngine from '@/lib/sketch-engine'
 export default {
   props: {
     word: {
-      type: Object
+      type: Object,
     },
     text: {
-      type: String
+      type: String,
     },
     level: {
-      default: 'outside'
-    }
+      default: 'outside',
+    },
   },
   data() {
     return {
@@ -86,36 +91,53 @@ export default {
       examples: undefined,
       concordanceKey: 0,
       words: [],
-      SketchEngine
+      SketchEngine,
     }
   },
   computed: {
     term() {
       return this.word ? this.word.bare : this.text
-    }
+    },
   },
   watch: {
     word() {
       this.update()
-    }
+    },
   },
   methods: {
     async update() {
       this.examples = undefined
-      let forms = this.word ? (await (await this.$dictionary).wordForms(this.word)).map(
-        form => form.form.replace(/'/g, '')
-      ) : []
+      let forms = this.word
+        ? (await (await this.$dictionary).wordForms(this.word)).map((form) =>
+            form.form.replace(/'/g, '')
+          )
+        : []
       this.words = [this.term].concat(forms)
-      this.examples = await SketchEngine.concordance({
+      let examples = await SketchEngine.concordance({
         term: this.term,
         l1: this.$l1,
-        l2: this.$l2
+        l2: this.$l2,
       })
+      for (let example of examples) {
+        if (this.$l2.code === 'zh') {
+          let sentences = example.l2.split(/(?<=[。！？：]+”?)/)
+          example.sentences = []
+          for (let sentence of sentences) {
+            let found = this.words.some((word) => sentence.includes(word))
+            if (found) {
+              example.sentences.push(sentence)
+            }
+          }
+        } else {
+          example.sentences = [example.l2]
+        }
+      }
+      this.examples = examples
       this.concordanceKey += 1
-    }
+    },
   },
   async mounted() {
     this.update()
-  }
+  },
 }
 </script>
