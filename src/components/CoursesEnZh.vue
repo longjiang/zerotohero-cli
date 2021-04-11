fc82571937faf4fa4b96bb29cac6b822c1083ee2
Jon Long <longjiang2005@gmail.com>
2021-04-10 22:32:38 -0700

    Change and add HSK course image.

--- src/components/CoursesEnZh.vue
+++ src/components/CoursesEnZh.vue
@@ -132,6 +132,26 @@
                       </h5>
                     </a>
                   </div>
+                  <div
+                    class="col-sm-6 col-md-4 mb-3"
+                  >
+                    <a
+                      target="_blank"
+                      :href="
+                        `https://chinesezerotohero.teachable.com/p/hsk-7-9-words`
+                      "
+                    >
+                      <img
+                        :src="`/img/courses/hsk7-9.jpg`"
+                        :alt="`HSK 7-9 Words`"
+                        class="img-fluid shadow rounded"
+                      />
+                      <h5 class="mt-2">
+                        HSK 7-9 Words
+                        <i class="fa fa-external-link-alt small ml-1" />
+                      </h5>
+                    </a>
+                  </div>
                   <div class="col-sm-6 col-md-4 mb-3">
                     <a
                       target="_blank"
